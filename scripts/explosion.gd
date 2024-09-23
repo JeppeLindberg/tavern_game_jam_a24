ea7d0544bbca785f4dc25da02d8664c0444f0b32
Jeppe Lindberg <jepp3441@gmail.com>
2024-09-23 20:22:28 +0200

    progress

--- scripts/explosion.gd
+++ scripts/explosion.gd
@@ -2,8 +2,12 @@ extends Area2D
 
 @onready var main:Node = get_node('/root/main')
 @onready var collider:CollisionShape2D = get_node('collider')
+@onready var sprite:Sprite2D = get_node('sprite')
 
-var _destroy_time: float
+var _spawn_time: float
+
+@export var animation_frames_per_second: float = 0.1
+@export var frames:Array = []
 
 func trigger():
 	var enemies = main.get_nodes_in_shape(collider, '', main.enemy_layer)
@@ -11,10 +15,13 @@ func trigger():
 		if enemy.has_method('take_damage'):
 			enemy.take_damage(5);
 
-	_destroy_time = main.curr_secs() + 0.1
+	_spawn_time = main.curr_secs()
 
 func _process(_delta: float) -> void:
-	if _destroy_time > 0:
-		if main.curr_secs() > _destroy_time:
+	if _spawn_time > 0:
+		var frame = int((main.curr_secs() - _spawn_time) * animation_frames_per_second)
+		if frame >= len(frames):
 			queue_free()
+		else:
+			sprite.texture = frames[frame]
 
